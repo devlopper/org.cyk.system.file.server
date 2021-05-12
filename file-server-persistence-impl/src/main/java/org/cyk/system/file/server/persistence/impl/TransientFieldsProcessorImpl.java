2fec7a95d992f6f6cc6dd51bdc4327af5caaec12
Christian Yao Komenan <kycdev@gmail.com>
2021-05-12 18:15:16 +0000

    mycommitfromcommand

--- file-server-persistence-impl/src/main/java/org/cyk/system/file/server/persistence/impl/TransientFieldsProcessorImpl.java
+++ file-server-persistence-impl/src/main/java/org/cyk/system/file/server/persistence/impl/TransientFieldsProcessorImpl.java
@@ -7,16 +7,17 @@ import org.cyk.system.file.server.persistence.api.query.FileQuerier;
 import org.cyk.system.file.server.persistence.entities.File;
 import org.cyk.utility.__kernel__.collection.CollectionHelper;
 import org.cyk.utility.__kernel__.file.FileHelper;
+import org.cyk.utility.persistence.query.Filter;
 
 @org.cyk.system.file.server.annotation.System
 public class TransientFieldsProcessorImpl extends org.cyk.utility.persistence.server.TransientFieldsProcessorImpl implements Serializable {
 
 	@Override
-	protected void __process__(Class<?> klass,Collection<?> objects, Collection<String> fieldsNames) {
+	protected void __process__(Class<?> klass,Collection<?> objects,Filter filter, Collection<String> fieldsNames) {
 		if(File.class.equals(klass))
 			processFiles(CollectionHelper.cast(File.class, objects),fieldsNames);
 		else
-			super.__process__(klass,objects, fieldsNames);
+			super.__process__(klass,objects,filter, fieldsNames);
 	}
 	
 	public void processFiles(Collection<File> files,Collection<String> fieldsNames) {
